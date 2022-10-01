c595230f0d1095fbe024d35454ce328211984524
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-10-02 01:21:51 +0530

    Edit Address

--- Adress_Book.java
+++ Adress_Book.java
@@ -1,67 +1,110 @@
 package com.bl.Address;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Adress_Book {
-	LinkedList<Contacts> contacts = new LinkedList<>();
-	Contacts contact  = new Contacts();
 	Scanner sc = new Scanner(System.in);
-//	contact.add("Contacts");
+	
+	ArrayList<Contacts> personDetails = new ArrayList<>();
 
-	public void add() {
+	
+	public void newContact() {
+		Contacts add = new Contacts();
+		System.out.println("Enter your First Name");
+		add.firstName = sc.nextLine();
+		System.out.println("Enter your Last Name");
+		add.lastName = sc.nextLine();
+		System.out.println("Enter your City");
+		add.city = sc.nextLine();
+		System.out.println("Enter your state");
+		add.state = sc.nextLine();
+		System.out.println("Enter your Zip");
+		add.zip = sc.nextInt();
+		System.out.println("Enter your Phone Number");
+		add.phNo = sc.nextInt();
+		System.out.println("Enter your email ID");
+		add.email = sc.next();
+		System.out.println("Added Successfully");
 
+		
+		personDetails.add(add);
 
-
-
-		System.out.println("Enter first Name");
-		String firstName=sc.next();
-		contact.setFirstName(firstName);
-
-		System.out.println("Enter last Name");
-		String lastName=sc.next();
-		contact.setLastName(lastName);
-		sc.nextLine();
-
-		System.out.println("Enter address");
-		String address=sc.nextLine();
-		contact.setAddress(address);
-
-		System.out.println("Enter city");
-		String city=sc.next();
-		contact.setCity(city);
-
-		System.out.println("Enter state");
-		String state=sc.next();
-		contact.setState(state);
-
-		System.out.println("Enter zip");
-		int zip=sc.nextInt();
-		contact.setZip(zip);
-
-		System.out.println("Enter phoneNo");
-		long phoneNo=sc.nextLong();
-		contact.setPhoneNo(phoneNo);
-
-		System.out.println("Enter email");
-		String email=sc.next();
-		contact.setEmail(email);
-
-		contacts.add(contact);
-
-
+		System.out.println(add);
 	}
-	public void showcontact()
-	{
-		 System.out.println(contact.toString()); 
+
+	
+	public void editPerson() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter name to Edit");
+		String name = sc.next();
+
+		for (int i = 0; i < personDetails.size(); i++) {
+			Contacts p = (Contacts) personDetails.get(i);
+
+			if (name.equals(p.getFirstName())) {
+				System.out.println(p);
+
+				System.out.print(
+						"Enter what to update(1.first name/ 2.last name/ 3.city/ 4.state/ 5.phone/ 6.zip/ 7.email): ");
+				sc.nextLine();
+				Integer choice = sc.nextInt();
+				switch (choice) {
+				case 1:
+					System.out.println("Enter your first name to update:");
+					p.firstName = sc.next();
+					break;
+				case 2:
+					System.out.println("Enter your last name to update:");
+					p.lastName = sc.next();
+					break;
+				case 3:
+					System.out.println("Enter your city name to update:");
+					p.city = sc.next();
+					break;
+				case 4:
+					System.out.println("Enter your state name to update:");
+					p.state = sc.next();
+					break;
+				case 5:
+					System.out.println("Enter Your phone no to update:");
+					p.phNo = sc.nextInt();
+					break;
+				case 6:
+					System.out.println("Enter Your zip to update:");
+					p.zip = sc.nextInt();
+					break;
+				case 7:
+					System.out.println("Enter Your email to update:");
+					p.email = sc.next();
+					break;
+				}
+				
+				personDetails.set(i, p);
+				System.out.println(p);
+			}
+		}
+		sc.close();
 	}
 
 	public static void main(String[] args) {
-		Adress_Book objadress = new Adress_Book();
-		objadress.add();
-		objadress.showcontact();
+		System.out.println("Welcome to Address Book Program ");
+		Scanner sc = new Scanner(System.in);
+
+		Adress_Book addPerson = new Adress_Book();
+		
+		System.out.println("Enter Number of persons you want to add : ");
+		int person_count = sc.nextInt();
+		for (int i = 0; i < person_count; i++) {
+			addPerson.newContact();
+		}
+		
+		addPerson.editPerson();
+
+		sc.close();
 	}
-	    }
+}
 	
 
 
